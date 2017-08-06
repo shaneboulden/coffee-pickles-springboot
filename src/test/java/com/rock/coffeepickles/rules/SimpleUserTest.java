4eb0604431177f139b5c3b9135611aec97503712
jockey10 <shane.boulden@gmail.com>
2017-08-06 11:56:04 +1000

    Add setup and tear-down

--- src/test/java/com/rock/coffeepickles/rules/SimpleUserTest.java
+++ src/test/java/com/rock/coffeepickles/rules/SimpleUserTest.java
@@ -3,6 +3,8 @@ package com.rock.coffeepickles.rules;
 import com.rock.coffeepickles.domain.Customer;
 import com.rock.coffeepickles.service.CoffeePriceService;
 import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -10,24 +12,31 @@ import static org.hamcrest.CoreMatchers.*;
 import com.rock.coffeepickles.domain.Coffee;
 
 import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 
 import java.math.BigDecimal;
 
 
 public class SimpleUserTest {
 
+    KieContainer container;
+
+    @Before
+    public void setup() {
+        KieServices services = KieServices.Factory.get();
+        container = services.newKieClasspathContainer();
+    }
+
     @Test
     public void everyFifthCoffeeIsFree() {
-        CoffeePriceService coffeePriceService = new CoffeePriceService(
-            KieServices.Factory.get().getKieClasspathContainer()
-        );
+        CoffeePriceService coffeePriceService = new CoffeePriceService();
         Customer user = new Customer("user1");
         // Create 4 coffees for the user
         for (int i=0; i < 4; i++) {
             user.purchaseCoffee(new Coffee(new BigDecimal("1.00"), user));
         }
 
-        Coffee coffee = coffeePriceService.getCoffeePrice(user);
+        Coffee coffee = coffeePriceService.getCoffeePrice(user,container);
 
         // After the rule fires, the coffee should be free
         assertThat(coffee.getPrice(), Matchers.comparesEqualTo(new BigDecimal("0.00")));
@@ -35,14 +44,17 @@ public class SimpleUserTest {
 
     @Test
     public void coffeeIsNormallyNotFree() {
-        CoffeePriceService coffeePriceService = new CoffeePriceService(
-                KieServices.Factory.get().getKieClasspathContainer()
-        );
+        CoffeePriceService coffeePriceService = new CoffeePriceService();
         Customer user = new Customer("user1");
 
-        Coffee coffee = coffeePriceService.getCoffeePrice(user);
+        Coffee coffee = coffeePriceService.getCoffeePrice(user, container);
 
         // The coffee should not be free
         assertThat(coffee.getPrice(), not(0.00));
+    }
+
+    @After
+    public void tearDown() {
+        container.dispose();
     }
 }
